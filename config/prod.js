d195d35cd91826d40bc8a20967c0603ab6db1332
Mendel Bakaleynik <mendelbak@gmail.com>
2020-07-21 18:51:03 +0300

    fixed typo in config/prod.js keys file

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  facebookClientID: process.env.FACBOOK_CLIENT_ID,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
   facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   githubClientID: process.env.GITHUB_CLIENT_ID,
   githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
