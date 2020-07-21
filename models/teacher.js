25e4084046b1ec4732d935b6d9330ba39bb61091
Mendel Bakaleynik <mendelbak@gmail.com>
2020-07-21 18:37:36 +0300

    removed practice model

--- models/teacher.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const teacherSchema = new Schema({
-    age: Number,
-    name: String,
-});
-
-mongoose.model('teachers', teacherSchema);
